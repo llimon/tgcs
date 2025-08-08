ee927495b858e23e8110bf828519fc309f1d668b
Luis E Limon <9660709+llimon@users.noreply.github.com>
2025-08-08 15:32:32 -0700

    Imported slang

---
+++ slang/build.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+# This is a buildpkg build.sh script
+# build.sh helper functions
+. ${BUILDPKG_SCRIPTS}/build.sh.functions
+#
+###########################################################
+# Check the following 4 variables before running the script
+topdir=slang
+version=2.2.3
+pkgver=1
+
+# https://www.jedsoft.org/releases/slang/old/slang-2.2.3.tar.gz
+source[0]=https://www.jedsoft.org/releases/${topdir}/old/${topdir}-${version}.tar.gz
+
+# If there are no patches, simply comment this
+#patch[0]=
+
+# Source function library
+. ${BUILDPKG_SCRIPTS}/buildpkg.functions
+
+# Redefin package global attributes
+pkgedby="Luis E Limon"
+pkgdirdesig="tgcsware"
+pkgprefix="TGCS"
+email="9660709+llimon@users.noreply.github.com"
+
+# Global settings
+export CPPFLAGS="-I$prefix/include"
+export LDFLAGS="-L$prefix/lib -R$prefix/lib"
+topsrcdir=${topdir}-${version}
+configure_args+=()
+
+reg prep
+prep()
+{
+    generic_prep
+    setdir source
+    ${__gsed} -i 's|^#! /bin/sh|#!/usr/tgcware/bin/bash|' configure
+
+}
+
+reg build
+build()
+{
+    generic_build
+}
+
+reg check
+check()
+{
+    generic_check
+}
+
+reg install
+install()
+{
+    generic_install DESTDIR
+    doc COPYING NEWS README UPGRADE.txt
+}
+
+reg pack
+pack()
+{
+    generic_pack
+}
+
+reg distclean
+distclean()
+{
+    clean distclean
+}
+
+###################################################
+# No need to look below here
+###################################################
+build_sh $*
